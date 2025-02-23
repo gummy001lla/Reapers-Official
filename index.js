41125c68faf6127ec68e9eb3d462bd31dea0b201
gummy001lla <mettaarida8@gmail.com>
2025-02-23 20:34:59 +0100

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Elite Services", "🎮 Playing Elite Services"];
+const statusMessages = ["🦺 Cheating With Reapers Official", "🎮 Playing Reapers Official"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
